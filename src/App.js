cc1411a99f2b9e933edef8b56ae5d032cc651247
uri.ashraf <uriashraf1@gmail.com>
2021-01-06 03:49:47 +0200

    done except exten 3

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
+import { BrowserRouter as Router, Link, Route} from 'react-router-dom'
+import Home from './components/Home';
+import About from './components/About';
+import Fentities from './components/Fentities';
+import Fentity from './components/Fentity';
 
 class App extends Component {
   constructor() {
@@ -21,15 +26,26 @@ class App extends Component {
   render() {
     const state = this.state
     return (
-      <div className="App">
-        <div id="home-background"></div>
-        <div id="main-links">
-          {/* Main Links */}
-        </div>
-        {/* Routes go here v */}
+      <Router>
+        <div className="App">
+          <div id="home-background"></div>
+          <div id="main-links">
+            {/* Main Links */}
+            <Link to="/">Home</Link>
+            <Link to="/about">About</Link>
+            
+            
+          </div>
+            {/* Routes go here v */}
+            <Route  exact path="/" component={Home}/>
+            <Route exact path="/about" render={() => <About items={Object.keys(state)} />} />
+            <Route path="/directory/:fentities" exact render={({ match }) => <Fentities match={match} state={state}/>}/>
+            <Route path="/directory/:fentities/:name" exact render={({ match }) => <Fentity match={match} state={state}/>}/>
 
-        {/* Routes go here ^ */}
-      </div>
+            {/* Routes go here ^ */}
+        </div>
+
+      </Router>
     );
   }
 }
--- src/components/Fentity.js
+++ src/components/Fentity.js
@@ -1,17 +1,33 @@
 import React, { Component } from 'react';
+import { Link, Redirect } from 'react-router-dom';
 import '../styles/fentity.css'
 
 
 class Fentity extends Component {
     render() {
+        console.log(this.props.match);
+        const {fentities, name} = this.props.match.params
+        const fentity= this.props.state[fentities].filter(f => {
+            return f.name.toLowerCase() === name.toLowerCase()
+        })[0]
+        console.log(fentity)
+        
         return (
-            <div id="creature-container">
-                <h1>{fentity.name}</h1>
-                <img src={fentity.imgUrl} alt=""/>
-                <div className="title">Power:</div>
-                <div className="power text"> {fentity.power}</div>
-                <div className="other text">{fentity.other}</div>
-            </div>
+            <React.Fragment>
+                {!fentity ? (
+                    <Redirect to="./"/>
+                ): (
+                <Link to={`/directory/${fentities}`}>
+                
+                    <div id="creature-container">
+                        <h1>{fentity.name}</h1>
+                        <img src={fentity.imgUrl} alt=""/>
+                        <div className="title">Power:</div>
+                        <div className="power text"> {fentity.power}</div>
+                        <div className="other text">{fentity.other}</div>
+                    </div>
+                </Link>)}
+            </React.Fragment>
         )
     }
 }
